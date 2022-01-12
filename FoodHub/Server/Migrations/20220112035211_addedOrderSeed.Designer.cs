0b29fb02b95626121b58d161ffa37146499358c1
Bel <bluhenin@outlook.com>
2022-01-12 11:53:05 +0800

    finished database

--- FoodHub/Server/Migrations/20220112035211_addedOrderSeed.Designer.cs
+++ FoodHub/Server/Migrations/20220112035211_addedOrderSeed.Designer.cs
@@ -4,14 +4,16 @@ using FoodHub.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FoodHub.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220112035211_addedOrderSeed")]
+    partial class addedOrderSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
